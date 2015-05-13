c1586cee4d8a8188894123467743c1564279a60f
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-05-13 21:18:45 +0800

    First commit, just begin to write

---
+++ gamelib/attack/Attack.java
@@ -0,0 +1,13 @@
+﻿public abstract class Attack {
+  private int MaxDamage = 0;
+  private int MinDamage = 0;
+  private int Probability = 0;
+  private int ColdTime = 0;
+  
+  public int getMaxDamage() {
+    return MaxDamage;
+  }
+  public void setMaxDamage(int maxDamage) {
+    MaxDamage = maxDamage;
+  }
+}
