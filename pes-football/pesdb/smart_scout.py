41b8057f819956fa3bd874ef1aa7e56e8eccf584
Zhaoqun Chen <zhaoqun.czq@antfin.com>
2019-05-21 17:29:48 +0800

    Add scouts search, given scouts you have, players that can be combined will be found. Using python smart_scout.py -f SOUNTS_FILE

--- pes-football/pesdb/smart_scout.py
+++ pes-football/pesdb/smart_scout.py
@@ -12,27 +12,66 @@ def loadScouts(dataPath):
     playerAbilities = json.loads(fin.read())
   return playerAbilities
 
+def loadCandicateScout(dataPath):
+  candicateScouts = []
+  with open(dataPath, 'r') as fin:
+    for line in fin:
+      candicateScouts.append(line.strip())
+  return candicateScouts
+
+# get scouts given player's name
 def getScoutsByName(scoutsMap, name):
   candicates = []
   idx = findName(map(lambda x:x['name'], scoutsMap), name)
   if idx >= 0:
     for scout in scoutsMap[idx]['scouts']:
-      candicates.append('%s  %s  %s  %s  %s' %(scoutsMap[idx]['name'], scout['rating'], scout['scouts'][0], scout['scouts'][1], scout['scouts'][2]))
+      candicates.append('%20s %3s %36s %36s %36s' %(scoutsMap[idx]['name'], scout['rating'], scout['scouts'][0], scout['scouts'][1], scout['scouts'][2]))
   return candicates
 
-def getScoutsByScout(scoutsMap, targetScounts):
+# get scouts combinations given parts of the scouts
+# may needs another scouts not in targetScouts
+def getScoutsByScout(scoutsMap, targetScouts):
   candicates = []
   for player in scoutsMap:
     for scout in player['scouts']:
       findTarget = True
-      for targetScout in targetScounts:
+      for targetScout in targetScouts:
         if targetScout not in ' '.join(scout['scouts']):
           findTarget = False
       if findTarget:
-        candicates.append('%s  %s  %s  %s  %s' %(player['name'], scout['rating'], \
+        candicates.append('%20s %3s %36s %36s %36s' %(player['name'], scout['rating'], \
                                      scout['scouts'][0], scout['scouts'][1], \
                                      scout['scouts'][2]))
   return candicates
+
+# get scouts combinations by candicateScouts only
+def getScoutsByScoutOnly(scoutsMap, candicateScouts):
+  candicateScoutsStr = ' '.join(candicateScouts)
+  fullCandicates = []
+  needOneCandicates = []
+  for player in scoutsMap:
+    for scout in player['scouts']:
+      if (scout['scouts'][0] in candicateScoutsStr or scout['scouts'][0] == '-') \
+        and (scout['scouts'][1] in candicateScoutsStr or scout['scouts'][1] == '-') \
+        and (scout['scouts'][2] in candicateScoutsStr or scout['scouts'][2] == '-'):
+        fullCandicates.append('%20s %3s %36s %36s %36s' %(player['name'], scout['rating'], \
+                                     scout['scouts'][0], scout['scouts'][1], \
+                                     scout['scouts'][2]))
+      elif scout['scouts'][0] in candicateScoutsStr and scout['scouts'][1] in candicateScoutsStr:
+        needOneCandicates.append('%20s %3s %36s %36s %36s - %36s' %(player['name'], scout['rating'], \
+                                     scout['scouts'][0], scout['scouts'][1], \
+                                     scout['scouts'][2], scout['scouts'][2]))
+      elif scout['scouts'][0] in candicateScoutsStr and scout['scouts'][2] in candicateScoutsStr:
+        needOneCandicates.append('%20s %3s %36s %36s %36s - %36s' %(player['name'], scout['rating'], \
+                                     scout['scouts'][0], scout['scouts'][1], \
+                                     scout['scouts'][2], scout['scouts'][1]))
+      elif scout['scouts'][1] in candicateScoutsStr and scout['scouts'][2] in candicateScoutsStr:
+        needOneCandicates.append('%20s %3s %36s %36s %36s - %36s' %(player['name'], scout['rating'], \
+                                     scout['scouts'][0], scout['scouts'][1], \
+                                     scout['scouts'][2], scout['scouts'][0]))
+      else:
+        pass
+  return fullCandicates, needOneCandicates
 
 def getScoutsNeedTwo(scoutsMap):
   candicates = []
@@ -56,15 +95,23 @@ def main():
     print('find scouts by name: %s' %(name))
     candicates = getScoutsByName(scoutsMap, name)
   elif 3 == len(sys.argv) and sys.argv[1] == '-s':
-    targetScounts = sys.argv[2].split('#')
-    print('find scouts by scout: %s' %(' + '.join(targetScounts)))
-    candicates = getScoutsByScout(scoutsMap, targetScounts)
+    targetScouts = sys.argv[2].split('#')
+    print('find scouts by scout: %s' %(' + '.join(targetScouts)))
+    candicates = getScoutsByScout(scoutsMap, targetScouts)
+  elif 3 == len(sys.argv) and sys.argv[1] == '-f':
+    candicateScouts = loadCandicateScout(sys.argv[2])
+    print('find play by candicate scout: %s' %(', '.join(candicateScouts)))
+    fullCandicates, needOneCandicates = getScoutsByScoutOnly(scoutsMap, candicateScouts)
+    print('-' * 30 + ' have all scouts ' + '-' * 30)
+    print('\n'.join(sortCandicates(fullCandicates)))
+    print('-' * 30 + ' have two scouts and need another one ' + '-' * 30)
+    print('\n'.join(sortCandicates(needOneCandicates)))
   elif 5 == len(sys.argv) and sys.argv[1] == '-p' and sys.argv[3] == '-s':
     name = sys.argv[2]
-    targetScounts = sys.argv[4].split('#')
-    print('find scouts by name %s and scout: %s' %(name, ' + '.join(targetScounts)))
+    targetScouts = sys.argv[4].split('#')
+    print('find scouts by name %s and scout: %s' %(name, ' + '.join(targetScouts)))
     candicatesByName = getScoutsByName(scoutsMap, name)
-    candicatesByScout = getScoutsByScout(scoutsMap, targetScounts)
+    candicatesByScout = getScoutsByScout(scoutsMap, targetScouts)
     candicates = list(set(candicatesByName).intersection(set(candicatesByScout)))
   else:
     candicates = getScoutsNeedTwo(scoutsMap)
