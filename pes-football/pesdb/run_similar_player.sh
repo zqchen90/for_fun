df6483a5f026aa33959e342cf089e05d25bb3985
Zhaoqun Chen <zhaoqun.czq@antfin.com>
2019-06-10 18:58:17 +0800

    New feature: find similar player by pca

---
+++ pes-football/pesdb/run_similar_player.sh
@@ -0,0 +1,8 @@
+python similar_player.py messi
+python similar_player.py cronaldo
+python similar_player.py lukaku
+python similar_player.py sergiobusquets
+python similar_player.py daviddegea
+python similar_player.py msalah
+python similar_player.py ehazard
+python similar_player.py nkante
